cd23c73e87c5f51b803ae6e05f9b01d9f254949a
thanhcongnguyen <nguyenthanhcong@unimarmores.com>
2018-07-22 02:03:16 +0700

    add users table

--- models/oauth_access_tokens.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var oauth_access_tokens = sequelize.define('oauth_access_tokens', {}, {});
-  oauth_access_tokens.associate = function(models) {
-    // associations can be defined here
-  };
-  return oauth_access_tokens;
-};
