aa2bf935ba5c3dabce010bddc401247cc44666f6
thanhcongnguyen <thanhcong150593@gmail.com>
2018-08-16 14:41:12 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cungnhauhoc.online' });
+  res.render('index', { title: 'cungnhauhoc.online' });
 });
 
 module.exports = router;
